5cae8d60577fdee968f6aff9c8ff4cc94d6b1c31
Pavel Krimont <kripalser@yandex.com>
2019-05-31 15:15:19 +0600

    Updates

---
+++ precache-manifest.da96a75c8b480ea62a6171d719a1ba7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19587b02f8f79938048c7f3d27d827b6",
+    "url": "/instareceptapp/index.html"
+  },
+  {
+    "revision": "cbaee697b6be1710ba79",
+    "url": "/instareceptapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/instareceptapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "cbaee697b6be1710ba79",
+    "url": "/instareceptapp/static/js/main.78ab5f04.chunk.js"
+  },
+  {
+    "revision": "8c6bb2264768af3dac63",
+    "url": "/instareceptapp/static/js/runtime~main.e3f9ba5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/instareceptapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
